5717cf7858d5a19ef0ce43a88896d0f28124f1b6
TimØ <Tim_thorndal@hotmail.com>
2021-09-08 14:17:40 +0200

    Updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,20 +2,20 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {                        //
-        zippo("rattle", 13);                             //1
-    }                                                               //15
-    public static void baffle(String blimp) {                       //
-        System.out.println(blimp);                                  //6
-        zippo("ping", -5);                               //7
-    }                                                               //11
-    public static void zippo(String quince, int flag) {             //
-        if (flag < 0) {                                             //2+8
-            System.out.println(quince + " zoop");                   //9
-        } else {                                                    //3
-            System.out.println("ik");                               //4
-            baffle(quince);                                         //5
-            System.out.println("boo-wa-ha-ha");                     //12
-        }                                                           //13
-    }                                                               //10
-}                                                                   //14
+    public static void main(String[] args) {                        //1
+        zippo("rattle", 13);                             //2
+    }                                                               //
+    public static void baffle(String blimp) {                       //8
+        System.out.println(blimp);                                  //9
+        zippo("ping", -5);                               //10
+    }                                                               //
+    public static void zippo(String quince, int flag) {             //3+11
+        if (flag < 0) {                                             //4+12
+            System.out.println(quince + " zoop");                   //13
+        } else {                                                    //5
+            System.out.println("ik");                               //6
+            baffle(quince);                                         //7
+            System.out.println("boo-wa-ha-ha");                     //14
+        }                                                           //
+    }                                                               //
+}                                                                   //
